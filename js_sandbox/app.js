d8514c7d79f84562429b00e7f03d52ef1de8cd89
Thomas Chouteau <tchouteau@tink.ca>
2018-02-15 08:59:57 -0500

    first commit

---
+++ js_sandbox/app.js
@@ -0,0 +1,58 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'This is a variable';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.error ('This is an error message');
+console.table({a:1, b:2});
+
+//Clear console
+console.clear();
+console.warn('This is a warning');
+
+// Cnsole time
+console.time('Time');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Time');
+
+
+// var, let, const
+
+// var name = 'Jonh Doe';
+// console.log(name);
+// name = 'Thomas Chouteau';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// //Multi word vars
+// var firstName = 'John'; //Camel case
+// var first_name = 'Emilie'; // Underscore
+
+// LET
+// var name = 'Jonh Doe';
+// console.log(name);
+// name = 'Thomas Chouteau';
+// console.log(name);
+
+// LET
+const name = 'Jonh Doe';
+console.log(name);
+// //Can not reassign
+// name = 'Sarah';
+// //Have to assign a value
+console.log(name);
